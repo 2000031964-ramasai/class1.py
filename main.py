ca1eb5953913464e0515d40ded0ab8524bf6253f
mahan <2000031964@kluniversity.in>
2022-01-20 13:55:08 +0530

    1st commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# cook your dish here
+import numpy as np
+
+arr = np.array([])
+
+n = int(input())
+while n > 0:
+    size = int(input())
+    for i in range(size):
+        np.append(arr, input())
+    max1 = arr[0]
+    for i in range(size):
+        if arr[i] > max1:
+            max1 = arr[i]
+
+    if max1 % 2 == 0:
+        print(max1 + 2)
+    else:
+        print(max1 + 1)
+
+    n = n - 1
+
+
+
